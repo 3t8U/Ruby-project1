a2303b4aa511c62b7539f668984c1e63835b177f
Student <example@example.com>
2019-12-13 09:28:59 -0800

    finish first length test

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,23 +1,31 @@
 class Anagram
 
-def same_length
+  def initialize(word)
+    @input = word1
+    @word2 = word2
+  end
+
+
+
+  def same_length (word)
+    if (@word1.split.length == @word2.split.length)
+      return true
+    else return false
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -6,26 +6,9 @@ require ('anagram.rb')
 
 describe('Anagram') do
   it( "take two words and identify if they are the same length") do
-          str = Finder.new("She sells sea shells by the sea shore")
-          expect(str.find_replace("sea", "seal")).to(eq("She sells seal shells by the seal shore"))
+           length = Anagram.new()
+          expect(@word1.split.length == @word2.split.length("tea", "eat")).to(eq("true"))
       end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-end
+    end
