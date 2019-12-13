1b24717aa5e6cb8fb52c333dfb13089a2d67458f
Student <example@example.com>
2019-12-13 14:06:44 -0800

    rewrite class and methods

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,9 +5,4 @@ require ('Anagram')
 
 
 
-describe('Anagram') do
-
-  it("Takes a string, converts it to lowercase, removes all whitespace ")
-  test = Anagram.new
-      expect('HORSE', 'CAT').to(eq("horse", "cat"))
-    end
+sdzklf'gadvl/kmv/.xc ?>X  s;dv
