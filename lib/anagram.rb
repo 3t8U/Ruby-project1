f8713920000bad685310dc1a16aab7c76e5213cd
Student <example@example.com>
2019-12-13 10:03:17 -0800

    start new direction with input files

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,30 @@
 class Anagram
 
   def initialize(word)
-    @input = word1
+    @word1 = word1
     @word2 = word2
   end
 
 
+def prepare_words
+  input_array = []
+  @word1.downcase.push("tea")
+  @word2.downcase.push("eat")
+puts input_array
 
-  def same_length (word)
+end
+
+
+
+
+
+
+
+
+
+  def same_length
+    @word1 = "tea"
+    @word2 = "eat"
     if (@word1.split.length == @word2.split.length)
       return true
     else return false
