e66174b80b61825df72ff43668940dc9631d0a9e
Nate Powers <russellsproutz@gmail.com>
2019-12-16 20:49:24 -0800

    no words

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,4 @@
+
 class Anagram
 
   def initialize(string1, string2)
@@ -11,14 +12,16 @@ class Anagram
       return false
     end
 
-  def basic (string1, string2)
-    @naked_word1 = string1.downcase.strip
-    @naked_word2 = string2.downcase.strip
-  end
-  # puts @nakedword2
-
-  def check_length
-    if (@naked_word1.length) != (@nakedword2.length)
-      return "Sorry, wrong number for an anagram"
+    def basic(string1, string2)
+      @naked_word1 = string1.downcase.strip
+      @naked_word2 = string2.downcase.strip
     end
-end 
+    # puts @nakedword
+
+    def check_length
+      if (@naked_word1.length) != (@nakedword2.length)
+        return "Sorry, wrong number for an anagram"
+      end
+    end
+  end #why?
+end #why?
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,14 +1,13 @@
-require ('rspec')
-require ('Anagram')
-require ('pry')
+require 'rspec'
+require 'Anagram'
+require 'pry'
 
 
 
 
 describe('#Anagram') do
   it("Takes a string, converts it to lowercase, removes all whitespace ") do
-  test = Anagram.new("HORse", "cAt")
-      expect(basic()).to(eq("horse", "cat"))
+  stripped = Anagram.new("HORse", "cAt")
+      expect(stripped.basic()).to(eq("horse","cat"))
     end
 end
- 
