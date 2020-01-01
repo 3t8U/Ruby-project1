8794eabafd6fc567da44052a71924e7c721e969f
Nate Powers <russellsproutz@gmail.com>
2020-01-01 00:40:55 -0800

    complete criteria tests

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,21 +1,40 @@
-
 class Anagram
+
+
 
 
   def initialize(string1, string2)
     @word1 = basic(string1)
     @word2 = basic(string2)
-    @input_array = [@word1, @word2]
+    @word_array = [@word1, @word2]
   end
 
-    def basic(word)
+  def basic(word)
     word.downcase.strip
-    end
+  end
 
-
-    def check_length
-      if (@word1.length) != (@word2.length)
-        return "Sorry, wrong number for an anagram"
+  def check_char
+    if (@word1.scan(/[!@#$%^&*()_+{}\[\]:;'"\/\\?><.,]/).empty?) && (@word2.scan(/[!@#$%^&*()_+{}\[\]:;'"\/\\?><.,]/).empty?)
+      return "winner, winner"
       end
+    return "bullshit"
     end
+
+  def check_length
+    if (@word1.length) != (@word2.length)
+      return "Sorry, wrong number for an anagram"
+    end
+  end
+
+  def v_check
+    if @word1.scan(/[aeiou]/).count >= 1 && @word2.scan(/[aeiou]/).count >= 1
+      return "You've got words"
+    else "Almost had me with them fancy letters"
+    end
+  end
+
+
+
+
+
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -6,8 +6,19 @@ require 'pry'
 
 
 describe('#Anagram') do
-  it("Takes a string, converts it to lowercase, removes all whitespace ") do
+
+  it("Checks inputed strings for special characters and returns warning if detected") do
+    out_of_place = Anagram.new("HOr$$e","c&t")
+        expect(out_of_place.check_char).to(eq("bullshit"))
+  end
+
+  it("Checks to see if both strings are the same length") do
   same_length = Anagram.new("HORse","cAt")
       expect(same_length.check_length).to(eq("Sorry, wrong number for an anagram"))
     end
+
+  it("Determines whether the inputed strings are actual words by checking for vowels") do
+    actual_word = Anagram.new("HORse","cAt")
+    expect(actual_word.v_check).to(eq("You've got words"))
+  end
 end
