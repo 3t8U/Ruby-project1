b73ef8d12c17d93912ce31dc496049871d3da591
Nate Powers <russellsproutz@gmail.com>
2020-01-02 16:20:54 -0800

    finish multi-word testing

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -10,7 +10,7 @@ describe('#Anagram') do
   end
 
   it("Checks to see if both strings are the same length") do
-    same_length = Anagram.new("HORse","cAt")
+    same_length = Anagram.new("HORse is","cAt")
     expect(same_length.check_length).to(eq("Sorry, wrong number for an anagram"))
   end
 
@@ -20,12 +20,22 @@ describe('#Anagram') do
   end
 
   it("Determines whether the inputed strings are anagrams") do
-    anagram_chk = Anagram.new("TaC","cAt")
+    anagram_chk = Anagram.new("TaC","CAt")
     expect(anagram_chk.ana_grams).to(eq("You're Anagramming!"))
   end
 
-  it("Determines whether the inputed strings are anagrams") do
+  it("Determines whether the inputed strings are antigrams") do
     antigram_chk = Anagram.new("TaC","cAt")
+    expect(antigram_chk.rebel_grams).to(eq("not and ANTIgram!"))
+  end
+
+  it("Determines whether multi-word inputed strings are anagrams") do
+    anagram_chk = Anagram.new("TaC bad"," daB CAt")
+    expect(anagram_chk.ana_grams).to(eq("You're Anagramming!"))
+  end
+
+  it("Determines whether the multi-word inputed strings are antigrams") do
+    antigram_chk = Anagram.new("TaC bad"," daB CAt")
     expect(antigram_chk.rebel_grams).to(eq("not and ANTIgram!"))
   end
 end
